d718bea65045549129e9a7c6afe20150a855fa07
Silva <oscaresilvae@hotmail.com>
2015-05-15 19:43:15 -0500

    First Commit

---
+++ Demo2Tejedoras/MainWindow.xaml.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Demo2Tejedoras.UC;
+using System.IO;
+
+namespace Demo2Tejedoras
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            this.Loaded += DemoMenu_Loaded;
+        }
+
+        private void DemoMenu_Loaded(object sender, RoutedEventArgs e)
+        {
+            UC.DemoMenu menu = new UC.DemoMenu();
+            menu.sociasClick += menu_sociasClick;
+            menu.legalClick += menu_legalClick;
+            menu.filoClick += menu_filoClick;
+            menu.actasClick += menu_actasClick;
+            GridMain.Children.Add(menu);
+        }
+
+        void menu_sociasClick(object sender, EventArgs e)
+        {
+            GridMain.Children.Clear();
+            UC.DemoSocias soci = new UC.DemoSocias();
+            GridMain.Children.Add(soci);
+        }
+
+        void menu_legalClick(object sender, EventArgs e)
+        {
+            GridMain.Children.Clear();
+            UC.DemoLegal lega = new UC.DemoLegal();
+            GridMain.Children.Add(lega);
+        }
+
+        void menu_filoClick(object sender, EventArgs e) 
+        {
+            GridMain.Children.Clear();
+            UC.DemoFilo fil = new UC.DemoFilo();
+            GridMain.Children.Add(fil);
+        }
+
+        void menu_actasClick(object sender, EventArgs e)
+        {
+            GridMain.Children.Clear();
+            UC.DemoActas acta = new UC.DemoActas();
+            GridMain.Children.Add(acta);
+        }
+    }
+}
---
+++ Demo2Tejedoras/UC/DemoMenu.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Demo2Tejedoras.UC
+{
+    /// <summary>
+    /// Interaction logic for DemoMenu.xaml
+    /// </summary>
+    public partial class DemoMenu : UserControl
+    {
+        public DemoMenu()
+        {
+            InitializeComponent();
+        }
+
+        public event EventHandler sociasClick;
+        public event EventHandler legalClick;
+        public event EventHandler filoClick;
+        public event EventHandler actasClick;
+
+        private void socias_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+          if(sociasClick!=null)
+          {
+              sociasClick(sender, null);
+          }
+        }
+
+        private void legal_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if(legalClick!=null)
+            {
+                legalClick(sender, null);
+            }
+        }
+
+        private void filosofía_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if(filoClick!=null)
+            {
+                filoClick(sender, null);
+            }
+        }
+
+        private void actas_MouseLeftButtonUp(object sender, MouseButtonEventArgs e)
+        {
+            if(actasClick!=null)
+            {
+                actasClick(sender, null);
+            }
+        }
+
+    }
+}
